87fd545055d048c65e22d32992754c3b904b4988
Rizko F Rachmayadi <63531054+rizko-d@users.noreply.github.com>
2023-11-15 20:48:16 +0700

    Update theSignal()Function.cpp

---
+++ theSignal()Function.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <csignal>
+#include <unistd.h>
+
+using namespace std;
+void signalHandler( int signum )
+{
+    cout << "Interrupt signal (" << signum << ") received.\n";
+    // cleanup and close up stuff here  
+    // terminate program  
+
+   exit(signum);  
+}
+int main () { 
+   // register signal SIGINT and signal handler  
+   signal(SIGINT, signalHandler);  
+
+   while(1){
+      cout << "Going to sleep...." << endl;
+      sleep(1);
+   }
+   return 0;
+}
